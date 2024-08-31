ab9cb2a04a3ba4e21a69fc1cce08614523b667f7
Triguna <swaint214@gmail.com>
2024-08-31 17:10:49 +0530

    Initialize project using Create React App and chapter-14: building youtube

---
+++ src/utils/constants.js
@@ -0,0 +1,3 @@
+const GOOGLE_API_KEY = "AIzaSyCczi_K4z1U47-3kC4XPofhiju2T-tdZGs"
+export const YOUTUBE_VIDEOS_API = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&&maxResults=50&regionCode=IN&key=${GOOGLE_API_KEY}`;
+
